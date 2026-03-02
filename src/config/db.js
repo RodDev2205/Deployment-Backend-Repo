f01936b0f1b82768b63453620a7efbc7ae249856
Rod Angelo <vendiolaangelo2@gmail.com>
2026-03-03 00:20:25 +0800

    Adjust db

--- src/config/db.js
+++ src/config/db.js
@@ -3,6 +3,15 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-console.log("DB URL:", process.env.DATABASE_URL);
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_PORT:", process.env.PORT);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_NAME:", process.env.DB_NAME);
 
-export const db = mysql.createPool(process.env.DATABASE_URL);
+export const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.PORT
+});
