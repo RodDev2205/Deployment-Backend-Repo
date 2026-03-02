9a123aa460f2670bae2246dbd2865f6e648c1a6f
Rod Angelo <vendiolaangelo2@gmail.com>
2026-03-03 00:28:50 +0800

    Adjust db

--- src/config/db.js
+++ src/config/db.js
@@ -11,7 +11,7 @@ console.log("DB_NAME:", process.env.DB_NAME);
 export const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASS,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT
 });
